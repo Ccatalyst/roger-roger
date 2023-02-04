402b152edf83a2a1ee3b1f32c67c11b799f49826
Matthew Todor <unclejunglegeorge@gmail.com>
2023-02-04 12:04:01 -0700

    added firebase app config information

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,13 @@ import "firebase/auth";
 import "firebase/storage";
 
 const firebaseConfig = {
-	// Add your config here
+	apiKey: "AIzaSyCfCXghSI2b7ISIma0To2fCZx7z5kr8eTQ",
+	authDomain: "roger-roger.firebaseapp.com",
+	projectId: "roger-roger",
+	storageBucket: "roger-roger.appspot.com",
+	messagingSenderId: "705289168669",
+	appId: "1:705289168669:web:17b59a5bc4dfe93d5ae3f4",
+	measurementId: "G-28BVRDRJZ9",
 };
 
 firebase.initializeApp(firebaseConfig);
