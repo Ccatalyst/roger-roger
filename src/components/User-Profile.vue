5ac2dbddb20ad6907d0b4444a657f2b7a6883e3e
Matthew Todor <unclejunglegeorge@gmail.com>
2023-02-21 09:17:49 -0700

    working on moving files to a script setup style to be more in line with documentation. Also, it makes it look more like react, which I'm used to

--- src/components/User-Profile.vue
+++ src/components/User-Profile.vue
@@ -1,31 +1,27 @@
 <template>
 	<div>
-		Logged in as {{ user.uid }}<br />
-		<button @click="signOut()" class="button">Log Out</button>
+		Logged in as {{ props.user.uid }}<br />
+		<button @click="signOutClick()" class="button">Log Out</button>
 	</div>
 </template>
 
-<script>
+<script setup>
+import { defineProps } from "vue";
+
 import { useFirebaseAuth } from "vuefire";
 import { signOut } from "@firebase/auth";
 const auth = useFirebaseAuth();
-export default {
-	data() {
-		return {
-			auth,
-		};
-	},
-	methods: {
-		// This is the method used via the @click on the button element
-		async signOut() {
-			try {
-				// This is the imported method from "@firebase/auth"
-				signOut(auth);
-			} catch (err) {
-				console.log(err);
-			}
-		},
-	},
-	props: ["user"],
+const props = defineProps({
+	user: String,
+});
+
+// This is the method used via the @click on the button element
+const signOutClick = async () => {
+	try {
+		// This is the imported method from "@firebase/auth"
+		await signOut(auth);
+	} catch (err) {
+		console.log(err);
+	}
 };
 </script>
--- src/components/User-State.vue
+++ src/components/User-State.vue
@@ -4,22 +4,14 @@
 	</div>
 </template>
 
-<script>
+<script setup>
+import { onUnmounted } from "vue";
 import { onAuthStateChanged } from "@firebase/auth";
 import { useFirebaseAuth, useCurrentUser } from "vuefire";
 const auth = useFirebaseAuth();
-export default {
-	setup() {
-		const user = useCurrentUser(auth);
-		const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => (user.value = firebaseUser));
-		return {
-			user,
-			unsubscribe,
-		};
-	},
 
-	unmounted() {
-		this.unsubscribe();
-	},
-};
+const user = useCurrentUser(auth);
+// const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => (user.value = firebaseUser));
+
+onUnmounted(() => onAuthStateChanged(auth, (firebaseUser) => (user.value = firebaseUser)));
 </script>
